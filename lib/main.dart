f67449fcd1a96c36bcafb459c797c502adc25d2a
Neil Gala <neilgala204@gmail.com>
2025-02-22 16:55:02 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:zync/screens/welcome_screen.dart';
+import 'package:zync/screens/login_screen.dart';
+import 'package:zync/screens/registration_screen.dart';
+import 'package:zync/screens/chat_screen.dart';
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
+      },
+    );
+  }
+}
+
+//theme: ThemeData.dark().copyWith(
+//         textTheme: TextTheme(
+//           bodyMedium: TextStyle(color: Colors.black54),
+//         ),
+//       ),
---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:zync/screens/login_screen.dart';
+import 'package:zync/screens/registration_screen.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:zync/components/rounded_button.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  static const String id = 'welcome_screen';
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Hero(
+              tag: 'logo',
+              child: Image.asset(
+                'images/logo.jpg',
+                height: 300.0,
+              ),
+            ),
+            RoundedButton(
+              color: Colors.lightBlueAccent,
+              title: 'Log In',
+              onPressed: () {
+                Navigator.pushNamed(
+                  context,
+                  LoginScreen.id,
+                );
+              },
+            ),
+            RoundedButton(
+              color: Colors.blueAccent,
+              title: 'Register',
+              onPressed: () {
+                Navigator.pushNamed(
+                  context,
+                  RegistrationScreen.id,
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
